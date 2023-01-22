91117ce652dbe059872d69606372202cc8bbb3f7
Firebelley <firebelley@users.noreply.github.com>
2023-01-22 11:55:53 -0500

    replace random audio stream player

--- node_library/scripts/random_audio_stream_player.gd
+++ node_library/scripts/random_audio_stream_player.gd
@@ -1,7 +1,8 @@
 @tool
-extends Node
+extends AudioStreamPlayer
+class_name RandomAudioStreamPlayer
 
-@export var streams: Array[AudioStream] = [] : get = get_streams, set = set_streams
+@export var streams: Array[AudioStream] = []
 @export var randomize_pitch: bool = false
 @export var pitch_minimum: float = .9
 @export var pitch_maximum: float = 1.1
@@ -13,95 +14,47 @@ func _ready():
 
 
 func _get_configuration_warnings():
-	if streams == null && get_child_count() > 0:
-		return "Stream array is out of sync with children. Children should be configured only through the streams property of %s." % name
-	if get_child_count() != streams.size():
-		return "Stream array is out of sync with children. Children should be configured only through the streams property of %s." % name
-	for i in range(get_child_count()):
-		var child = get_child(i)
-		if child.stream != streams[i]:
-			return "Node {child_name} is out of sync with the streams array of {root_name}.".format({ "child_name": child.name, "root_name": name })
-	return ""
+	var warnings: Array[String] = []
+	if stream != null:
+		warnings.append("The stream property has no effect on this node. Use the streams array to configure audio streams.")
+	if pitch_scale != 1 && randomize_pitch:
+		warnings.append("Randomized pitch scale will override the manually configured pitch scale.")
+	return warnings
 
 
-func play(): 
-	var valid_children = []
-	for child in get_children():
-		if !child.playing:
-			valid_children.append(child)
+func play_random():
+	play_random_exclude_streams()
+
+
+func play_random_exclude_streams(exclude_streams: Array[AudioStream] = []) -> RandomAudioStreamPlayer:
+	var adjusted_streams = streams
+	if exclude_streams.size() > 0:
+		adjusted_streams = adjusted_streams.filter(func(audio_stream: AudioStream): return !exclude_streams.has(audio_stream))
+
+	if adjusted_streams.size() == 0:
+		return null
 	
-	if valid_children.size() == 0:
-		return
-	
-	var random_index = rng.randi_range(0, valid_children.size() - 1)
-	var child = valid_children[random_index] as AudioStreamPlayer
+	var random_index = rng.randi_range(0, adjusted_streams.size() - 1)
+	var stream = adjusted_streams[random_index]
+
+	var instance = duplicate() as RandomAudioStreamPlayer
+	instance.stream = stream
 	if randomize_pitch:
-		child.pitch_scale = rng.randf_range(pitch_minimum, pitch_maximum)
-	child.play()
+		instance.pitch_scale = rng.randf_range(pitch_minimum, pitch_maximum)
 	
+	add_child(instance)
+	instance.finished.connect(on_audio_finished.bind(instance))
+	instance.play()
+	return instance
+
 
 func play_times(times: int):
+	var played_streams: Array[AudioStream] = []
 	for i in range(times):
-		play()
+		var audio_instance = play_random_exclude_streams(played_streams)
+		if audio_instance != null:
+			played_streams.append(audio_instance.stream)
 
 
-func remove_stream_player(node: AudioStreamPlayer):
-	node.owner = null
-	remove_child(node)
-	node.queue_free()
-
-
-func add_new_node(stream: AudioStream):
-	var root = get_tree().edited_scene_root
-	var new_node = AudioStreamPlayer.new()
-	add_child(new_node)
-	new_node.owner = root
-	new_node.stream = stream
-
-
-func update_stream_player_nodes():
-	if !Engine.is_editor_hint():
-		return
-
-	var existing_nodes = []
-	for node in get_children():
-		if (node.stream != null):
-			existing_nodes.append(node)
-		remove_child(node)
-		node.owner = null
-
-	for i in range(streams.size()):
-		var root = get_tree().edited_scene_root
-		if (streams[i] != null):
-			var existing_node = extract_first_node_with_stream(existing_nodes, streams[i].resource_path)
-			if existing_node != null:
-				add_child(existing_node)
-				existing_node.owner = root
-				existing_node.name = "AudioStreamPlayer%s" % (existing_node.get_index() + 1)
-			else:
-				add_new_node(streams[i])
-		else:
-			add_new_node(null)
-
-	for node in existing_nodes:
-		node.queue_free()
-
-
-func extract_first_node_with_stream(existing_nodes: Array, resource_path: String) -> AudioStreamPlayer:
-	var found_node: AudioStreamPlayer = null
-	for i in range(existing_nodes.size()):
-		var node = existing_nodes[i]
-		if (node.stream != null && node.stream.resource_path == resource_path):
-			found_node = node
-			existing_nodes.remove_at(i)
-			break
-	return found_node
-
-
-func set_streams(new_streams):
-	streams = new_streams
-	update_stream_player_nodes()
-
-
-func get_streams():
-	return streams
+func on_audio_finished(stream_player: RandomAudioStreamPlayer):
+	stream_player.queue_free()
