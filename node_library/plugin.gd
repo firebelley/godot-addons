23ad9554779fa89bc4e12f1c3ee676d357cee011
Firebelley <firebelley@users.noreply.github.com>
2023-01-22 12:21:14 -0500

    update plugin

--- node_library/plugin.gd
+++ node_library/plugin.gd
@@ -14,7 +14,7 @@ const SETTING_ENABLE_SCREEN_TRANSITION = "addons/node_library/enable_screen_tran
 
 
 func _enter_tree():
-	add_custom_type(RANDOM_AUDIO_STREAM_PLAYER, "Node", preload("scripts/random_audio_stream_player.gd"), preload("assets/icons/random_audio_stream_player_16x16.png"))
+	add_custom_type(RANDOM_AUDIO_STREAM_PLAYER, "AudioStreamPlayer", preload("scripts/random_audio_stream_player.gd"), preload("assets/icons/random_audio_stream_player_16x16.png"))
 	add_custom_type(RANDOM_AUDIO_STREAM_PLAYER_2D, "Node2D", preload("scripts/random_audio_stream_player_2d.gd"), preload("assets/icons/random_audio_stream_player_2d_16x16.png"))
 	add_custom_type(RANDOM_TIMER, "Timer", preload("scripts/random_timer.gd"), preload("assets/icons/random_timer_16x16.png"))
 	add_custom_type(SHAKY_CAMERA_2D, "Camera2D", preload("scripts/shaky_camera_2d.gd"), null)
