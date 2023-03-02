bd15c5e23c249ad24d7b5e5a5bb81672157bcf53
Firebelley <firebelley@users.noreply.github.com>
2023-03-01 19:59:13 -0500

    updates for godot 4

--- node_library/plugin.gd
+++ node_library/plugin.gd
@@ -6,14 +6,9 @@ const RANDOM_AUDIO_STREAM_PLAYER_2D = "RandomAudioStreamPlayer2D"
 const RANDOM_TIMER = "RandomTimer"
 const SHAKY_CAMERA_2D = "ShakyCamera2D"
 
-const AUTOLOAD_SCREEN_TRANSITION = "ScreenTransition"
-
-const SETTING_ENABLE_SCREEN_TRANSITION = "addons/node_library/enable_screen_transition"
-
-
 func _enter_tree():
 	add_custom_type(RANDOM_AUDIO_STREAM_PLAYER, "AudioStreamPlayer", preload("scripts/random_audio_stream_player.gd"), preload("assets/icons/random_audio_stream_player_16x16.png"))
-	add_custom_type(RANDOM_AUDIO_STREAM_PLAYER_2D, "Node2D", preload("scripts/random_audio_stream_player_2d.gd"), preload("assets/icons/random_audio_stream_player_2d_16x16.png"))
+	add_custom_type(RANDOM_AUDIO_STREAM_PLAYER_2D, "AudioStreamPlayer2D", preload("scripts/random_audio_stream_player_2d.gd"), preload("assets/icons/random_audio_stream_player_2d_16x16.png"))
 	add_custom_type(RANDOM_TIMER, "Timer", preload("scripts/random_timer.gd"), preload("assets/icons/random_timer_16x16.png"))
 	add_custom_type(SHAKY_CAMERA_2D, "Camera2D", preload("scripts/shaky_camera_2d.gd"), null)
 
