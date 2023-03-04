660bf7de61146f6a7894cb4b0cb022a2aea9c202
Firebelley <firebelley@users.noreply.github.com>
2023-03-04 10:10:11 -0500

    add limit to random audio stream players

--- node_library/scripts/random_audio_stream_player.gd
+++ node_library/scripts/random_audio_stream_player.gd
@@ -5,6 +5,11 @@ extends AudioStreamPlayer
 @export var randomize_pitch: bool = false
 @export var pitch_minimum: float = .9
 @export var pitch_maximum: float = 1.1
+@export_category("Limit Simultaneous Plays")
+## If greater than 0, limits the number of playing sounds to this number.
+@export var max_playing: int = 0
+## Every RandomAudioStreamPlayer with the same ID will count toward the max_playing count.
+@export var max_playing_id: String = null
 
 var rng = RandomNumberGenerator.new()
 
@@ -19,7 +24,13 @@ func _get_configuration_warnings():
 		warnings.append("The stream property has no effect on this node. Use the streams array to configure audio streams.")
 	if pitch_scale != 1 && randomize_pitch:
 		warnings.append("Randomized pitch scale will override the manually configured pitch scale.")
+	if max_playing > 0 && (max_playing_id == "" || max_playing_id == null):
+		warnings.append("max_playing is configured but no ID has been supplied. This may produce undesirable results.")
 	return warnings
+
+
+func get_max_playing_id():
+	return "RandomAudioStreamPlayer2D_%s" % max_playing_id
 
 
 func play_random():
@@ -33,6 +44,9 @@ func play_random_exclude_streams(exclude_streams: Array[AudioStream] = []) -> Au
 
 	if adjusted_streams.size() == 0:
 		return null
+
+	if max_playing > 0 && get_tree().get_nodes_in_group(get_max_playing_id()).size() >= max_playing:
+		return null
 	
 	var random_index = rng.randi_range(0, adjusted_streams.size() - 1)
 	var stream = adjusted_streams[random_index]
@@ -41,9 +55,12 @@ func play_random_exclude_streams(exclude_streams: Array[AudioStream] = []) -> Au
 	instance.stream = stream
 	instance.volume_db = volume_db
 	instance.pitch_scale = pitch_scale
-	instance.mix_target = mix_target
 	instance.max_polyphony = max_polyphony
+	instance.mix_target = mix_target
 	instance.bus = bus
+
+	if max_playing > 0:
+		instance.add_to_group(get_max_playing_id())
 
 	if randomize_pitch:
 		instance.pitch_scale = rng.randf_range(pitch_minimum, pitch_maximum)
